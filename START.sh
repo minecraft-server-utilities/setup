2f48a69a12088332f98fa66ccce607b399367010
AntonyMo <32247865+AntonyMoes@users.noreply.github.com>
2021-09-26 19:01:20 +0300

    Add setup to start script

--- START.sh
+++ START.sh
@@ -1,3 +1,30 @@
 #!/bin/bash
-java -Xmx4096M -Xms1024M -jar server.jar -nogui
 
+error() {
+  cat <<< "$@" 1>&2;
+  exit 1
+}
+
+backup=$1
+if [ -f "$backup" ]
+then
+  rm -rf world/
+  if ! tar -xzf "$backup";
+  then
+    error "Could not extract \"$backup\"!"
+  fi
+else
+  error "File \"$backup\" not found!"
+fi
+
+# TODO: check java version and set it
+if ! java -version;
+then
+  # TODO: add package manager choice
+  if ! sudo apt-get install openjdk-8-jdk -y;
+  then
+    error "Could not install missing openjdk!"
+  fi
+fi
+
+java -Xmx7168M -Xms2048M -jar server.jar -nogui
